283648b124163061947d5b5e8336163df261ce15
Vishnu Prakash <prakashvishnu857@gmail.com>
2025-07-21 18:46:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+        let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+});
+
+    
